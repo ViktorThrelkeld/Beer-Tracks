5848feef113825cd18bddc8e2c80d3b4b0925c53
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2014-01-15 10:04:24 -0600

    finish readme

---
+++ data/data.rb
@@ -0,0 +1,74 @@
+# the following is an example of data from brewery.db.
+#I might pull the ABV from here. on or around line 19.
+#Unfortunately they don't show calories
+{
+  "status" : "success",
+  "data" : [
+    {
+      "isOrganic" : "N",
+      "labels" : {
+        "medium" : "http://s3.amazonaws.com/",
+        "large" : "http://s3.amazonaws.com/",
+        "icon" : "http://s3.amazonaws.com/"
+      },
+      "status" : "verified",
+      "id" : "li0U6A",
+      "updateDate" : "2012-03-27 01:50:11",
+      "beerVariation" : {
+        "id" : "cqsB6i",
+        "abv" : "7.5",
+        "description" : "Jai Alai IPA is a monster interpretation of an American IPA. In fact, it is so big that it equals the alcohol of some double IPAs on the market. Our IPA uses 6 different hop varietals, with Simcoe hops only being used for dry hopping. The rest of the hop additions are blended at different IBU's (International Bittering Units) in groups of three hops per addition in order to create more hop complexity. At CCB, we love hops but also feel that balance is important for IPAs. So Jai Alai features a sturday caramel malt component which helps to create balance, staving off hop astringency. Ultimately, Jai Alai is a very strong interpretation of a single American IPA. We hope that it makes Tampa Bay natives happy because this is head brewer Wayne Wamble's favorite Cigar City beer and he'd love to share one with you!",
+        "name" : "Jai Alai IPA",
+        "glass" : {
+          "id" : 5,
+          "createDate" : "2012-01-03 03:41:33",
+          "name" : "Pint"
+        },
+        "style" : {
+          "id" : 30,
+          "category" : {
+            "id" : 3,
+            "createDate" : "2012-03-21 21:06:45",
+            "name" : "North American Origin Ales"
+          },
+          "srmMax" : "14",
+          "ibuMax" : "70",
+          "srmMin" : "6",
+          "description" : "American-style India pale ales are perceived to have medium-high to intense hop bitterness, flavor and aroma with medium-high alcohol content. The style is further characterized by floral, fruity, citrus-like, piney, resinous, or sulfur-like American-variety hop character. Note that one or more of these American-variety hop characters is the perceived end, but the hop characters may be a result of the skillful use of hops of other national origins. The use of water with high mineral content results in a crisp, dry beer. This pale gold to deep copper-colored ale has a full, flowery hop aroma and may have a strong hop flavor (in addition to the perception of hop bitterness). India pale ales possess medium maltiness which contributes to a medium body. Fruity-ester flavors and aromas are moderate to very strong. Diacetyl can be absent or may be perceived at very low levels. Chill and/or hop haze is allowable at cold temperatures. (English and citrus-like American hops are considered enough of a distinction justifying separate American-style IPA and English-style IPA categories or subcategories. Hops of other origins may be used for bitterness or approximating traditional American or English character. See English-style India Pale Ale",
+          "fgMin" : "1.012",
+          "ibuMin" : "50",
+          "createDate" : "2012-03-21 21:06:45",
+          "fgMax" : "1.018",
+          "abvMax" : "7.5",
+          "ogMin" : "1.06",
+          "abvMin" : "6.3",
+          "name" : "American-Style India Pale Ale",
+          "categoryId" : 3
+        },
+        "createDate" : "2012-01-03 03:43:31",
+        "labels" : {
+          "medium" : "http://s3.amazonaws.com/",
+          "large" : "http://s3.amazonaws.com/",
+          "icon" : "http://s3.amazonaws.com/"
+        },
+        "availableId" : 1,
+        "styleId" : 30,
+        "updateDate" : "2012-03-27 01:49:05",
+        "glasswareId" : 5,
+        "available" : {
+          "id" : "1",
+          "description" : "Available year round as a staple beer.",
+          "name" : "Year Round"
+        },
+        "isOrganic" : "N",
+        "status" : "verified",
+        "statusDisplay" : "Verified"
+      },
+      "statusDisplay" : "Verified",
+      "createDate" : "2012-03-27 01:50:08",
+      "name" : "Jai Alai IPA (Aged in White Oak)",
+      "beerVariationId" : "cqsB6i"
+    }
+  ],
+  "message" : "Request Successful"
+}
