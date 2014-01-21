d776896b7655302fc142e53d31ea2a316c113e78
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2014-01-21 11:04:07 -0600

    add rakefileand helper

---
+++ test/test_entering.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+
+class TestEntering < MiniTest::Unit::TestCase
+
+  def test_something
+    assert false, "Missing test implementation"
+   end
+
+  def test_valid_purchase_gets_saved
+    assert false, "Missing test implementation"
+  end
+
+  def test_invalid_purchase_doesnt_get_saved
+    assert false, "Missing test implementation"
+  end
+
+  def test_error_message_for_missing_tests
+    assert false, "Missing test implementation"
+  end
+
+  def test_error_message_for_missing_name
+    result = "./beertracks add"
+    assert_equal "You must provide the name of the product you are adding", result
+  end
+
+
+
+end
