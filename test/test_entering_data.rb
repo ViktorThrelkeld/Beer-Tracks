a0d2981556a6b1208a37c3ee5c8a39e7e47b8f72
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2014-01-21 14:55:33 -0600

    Finish Implementation of AddError Messages

--- test/test_entering_data.rb
+++ test/test_entering_data.rb
@@ -2,17 +2,42 @@ require_relative 'helper'
 
 class TestEnteringData < MiniTest::Unit::TestCase
 
-  def test_something
-    assert true
+  def test_valid_entry_gets_saved
+    skip "needs implementation"
+    assert false, "Missing test implementation"
   end
 
-  def test_something_too
-    assert true
+  def test_invalid_entry_doesnt_get_saved
+    skip "needs implementation"
+    assert false, "Missing test implementation"
   end
 
-  def test_error_message_for_missing_name
+  def test_error_message_for_missing_cost
+    skip "needs implementation"
+    assert false, "Missing test implementation"
+  end
+
+  def test_error_message_for_cost
+    command = "./beertracks add YazooPale --oz 12"
+    expected = "You must provide the cost of the beer you drank."
+    assert_command_output expected, command
+  end
+
+  def test_error_message_for_missing_oz_and_cost
+    command = "./beertracks add YazooPale"
+    expected = "You must provide the cost and total ounces of the beer you drank."
+    assert_command_output expected, command
+  end
+
+  def test_error_message_for_missing_oz
+    command = "./beertracks add YazooPale --cost 5"
+    expected = "You must provide the total ounces of the beer you drank."
+    assert_command_output expected, command
+  end
+
+  def test_error_message_for_missing_beer_name
     command = "./beertracks add"
-    expected = "You must provide the name of the product you are adding."
+    expected = "You must provide the name of the beer you drank."
     assert_command_output expected, command
   end
 end
