f89db93ca6f5cdbea96ff53df5b4a29e5c8afb75
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2014-01-29 14:12:31 -0600

    refactor import

---
+++ test/test_importing_entries.rb
@@ -0,0 +1,48 @@
+require_relative 'helper'
+require_relative '../lib/importer'
+
+class TestImportingEntries < BeerTest
+  def import_data
+    Importer.import("test/sample_entries_data.csv")
+  end
+
+  def test_the_correct_number_of_entries_are_imported
+    skip
+    import_data
+    assert 4, Entries.all.count
+  end
+
+  def test_entries_are_imported_fully
+    skip
+    import_data
+    expected = ["Guinness Draught, 20, 5.00, stout",
+    "Bush, 25, 5.00, pilsner",
+    "Heineken, 12, 5.00, pilsner",
+    "Anchor Steam, 12, 5.00, porter"]
+    actual = Entries.all.map do |beer|
+      "#{beer.name}, #{beer.ounces}, #{beer.cost}, #{beer.category.name}"
+    end
+    assert_equal expected, actual
+  end
+
+  def test_extra_categories_arent_created
+    skip
+    import_data
+    assert 4, Category.all.count
+  end
+
+  def test_categories_are_created_as_needed
+    skip
+    Category.create("stout")
+    Category.create("Pets")
+    import_data
+    assert 4, Category.all.count
+  end
+
+  def test_data_isnt_duplicated
+    skip
+    import_data
+    expected = ["stout", "pilsner", "porter", "IPA"]
+    assert_equal expected, Category.all.map(&:name)
+  end
+end
